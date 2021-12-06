55f68caf169051ed7f4a0876d0fc8077cfb4da1d
nlaris <nick@larisnet.com>
2021-12-06 12:23:45 -0500

    simplified

--- src/Day6.java
+++ src/Day6.java
@@ -2,41 +2,33 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 public class Day6 {
 
     private static final String INPUT_FILE = "day6.txt";
     private static final int NUM_DAYS = 256;
-    private static List<Integer> allSpawns = new ArrayList<>();
-    private static final HashMap<Integer, Long> productionValues = new HashMap<>();
+    private static List<Integer> initialSpawns = new ArrayList<>();
+    private static long[] spawns = new long[9];
 
     public static void main(String[] args) throws IOException {
         readInput();
-        long totalSpawns = 0;
-        for (Integer spawn : allSpawns) {
-            if (!productionValues.containsKey(spawn)) {
-                long productionValue = getProductionValue(spawn);
-                productionValues.put(spawn, productionValue);
-            }
-            totalSpawns += productionValues.get(spawn);
+        for(Integer spawn : initialSpawns) {
+            spawns[spawn]++;
         }
-        System.out.println(totalSpawns);
+        System.out.println(getTotalSpawns());
     }
 
     private static void readInput() throws IOException {
         try (BufferedReader br = new BufferedReader(new FileReader(INPUT_FILE))) {
-            allSpawns = Arrays.stream(br.readLine().split(","))
+            initialSpawns = Arrays.stream(br.readLine().split(","))
                     .map(Integer::parseInt)
                     .collect(Collectors.toList());
         }
     }
 
-    private static long getProductionValue(final int initialValue) {
+    private static long getTotalSpawns() {
         int daysRemaining = NUM_DAYS;
-        long[] spawns = new long[9];
-        spawns[initialValue] = 1;
         while (daysRemaining > 0) {
             long[] newSpawns = new long[9];
             for (int i = spawns.length - 1; i >= 0; i--) {
