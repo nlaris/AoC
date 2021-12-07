01283bb276dc33a12b5ce70e42d388b19db01c8d
nlaris <nick@larisnet.com>
2021-12-07 11:05:50 -0500

    smarter Day 7

--- src/Day7.java
+++ src/Day7.java
@@ -7,7 +7,6 @@ import java.util.stream.Collectors;
 public class Day7 {
 
     private static final String INPUT_FILE = "day7.txt";
-    private static final HashMap<Integer, Integer> distanceScores = new HashMap<>();
     private static List<Integer> inputs = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
@@ -16,22 +15,10 @@ public class Day7 {
         int medianIndex = inputs.size() / 2;
         System.out.println("Part 1: " + getPart1PositionScore(inputs.get(medianIndex)));
 
-        // start at the average position
-        float inputSum = inputs.stream().reduce(0, Integer::sum);
-        int currentPosition = Math.round(inputSum / inputs.size());
-        int currentScore = getPart2PositionScore(currentPosition);
-        final int direction = getPart2PositionScore(currentPosition + 1) < currentScore ? 1 :
-                getPart2PositionScore(currentPosition - 1) < currentScore ? -1 : 0;
-
-        if (direction != 0) {
-            int newScore = currentScore - 1;
-            while (newScore < currentScore) {
-                currentScore = newScore;
-                currentPosition += direction;
-                newScore = getPart2PositionScore(currentPosition);
-            }
-        }
-        System.out.println("Part 2: " + currentScore);
+        float averageInput = inputs.stream().reduce(0, Integer::sum).floatValue() / inputs.size();
+        int aveFloorScore = getPart2PositionScore((int)Math.floor(averageInput));
+        int aveCeilScore = getPart2PositionScore((int)Math.ceil(averageInput));
+        System.out.println("Part 2: " + (Math.min(aveCeilScore, aveFloorScore)));
     }
 
     private static int getPart1PositionScore(final int position) {
@@ -43,13 +30,7 @@ public class Day7 {
     }
 
     private static int getDistanceFuel(final int n) {
-        if (n <= 1)
-            return n;
-        if (!distanceScores.containsKey(n)) {
-            final int score = n + getDistanceFuel(n - 1);
-            distanceScores.put(n, score);
-        }
-        return distanceScores.get(n);
+        return (n * (n+1) / 2);
     }
 
     private static void readInput() throws IOException {
